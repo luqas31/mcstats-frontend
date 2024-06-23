248ea57f50d68e6285c6f1030859714edbc945f4
luqas31 <lucas.ers@icloud.com>
2024-06-23 16:42:07 +0100

    add login status

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -8,6 +8,7 @@ function Login() {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 
+	const [loginStatus, setLoginStatus] = useState('');
 
 	const navigate = useNavigate();
 
